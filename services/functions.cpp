6280fec7c79fab8f8a1289c1b7576773f16f99f4
nikhilks-005 <nikhilks858@gmail.com>
2026-02-12 17:25:24 +0530

    Initial commit

--- services/functions.cpp
+++ services/functions.cpp
@@ -2,14 +2,14 @@
 using namespace std;
 
 void hi() {
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < 5; i++) {
         cout << "hi: i = " << i << endl;
     }
 }
 
 void hello() {
     // for (int i = 0; i < 5; i++)
-    for (int i=0;i<5;i++)
+    for (int i=0;i<10;i++)
     {
         cout << "hello: i = " << i << endl;
     }
